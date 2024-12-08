dcdc019d6f51441a9e208bf93a955e216819beea
Gabriel Klein <gabrielklein289@hotmail.com>
2024-12-08 11:59:19 -0300

    fix: adjust to get the right connection

--- src/main/java/br/com/gklein/livechat/config/WebSocketConfig.java
+++ src/main/java/br/com/gklein/livechat/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/live-chat").withSockJS();
+        registry.addEndpoint("/live-chat");
     }
 }
