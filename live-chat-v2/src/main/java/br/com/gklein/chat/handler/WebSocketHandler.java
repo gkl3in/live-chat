41aaf10de9fb4002096707252609b7693c7a4586
Gabriel Klein <gabrielklein289@hotmail.com>
2025-01-19 12:42:23 -0300

    feat: Created the html

--- live-chat-v2/src/main/java/br/com/gklein/chat/handler/WebSocketHandler.java
+++ live-chat-v2/src/main/java/br/com/gklein/chat/handler/WebSocketHandler.java
@@ -6,12 +6,29 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         System.out.println("[afterConnectionEstablished] session id " + session.getId());
+
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    session.sendMessage(new TextMessage("Olá " + UUID.randomUUID()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 2000L, 2000L);
     }
 
     @Override
